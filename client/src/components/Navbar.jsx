7806d0dd4d1040ba7b4dd8ccd2074e0d191f6882
ThilinaJayamal <thilinajayamal271@gmail.com>
2025-10-05 21:07:25 +0530

    clenup style

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { LogOut, MessageSquare, MessageSquareIcon, Settings, User } from 'lucide-react'
+import { Hand, LogOut, Settings, User } from 'lucide-react'
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useAuthStore } from '../store/authStore'
@@ -13,9 +13,9 @@ function Navbar() {
           <div className='flex items-center gap-8'>
             <Link to={"/"} className='flex items-center gap-2.5 hover:opacity-80 transition-all'>
               <div className='w-9 h-9 rounded-lg bg-primary/10 flex items-center justify-center'>
-                <MessageSquare className='w-5 h-5 text-primary' />
+                <Hand className='w-5 h-5 text-primary' />
               </div>
-              <h1 className='text-lg font-bold'>Chatty</h1>
+              <h1 className='text-lg font-bold'>SayHi</h1>
             </Link>
           </div>
 
